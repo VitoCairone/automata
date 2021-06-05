4d5c7a6238f5f0c4b370c14016dc1c0dde64b5de
Vito Cairone <vitocairone@gmail.com>
2021-06-05 15:46:49 -0700

    add test cases

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ function createGrid(n_i, n_j, fnGridConstructor) {
         grid.cells.push(row);
     }
 
+    assignNeighbors(grid);
+
     return grid;
 }
 
@@ -51,13 +53,44 @@ function showGrid(grid) {
     };
 }
 
+function showGridAs2dArr(grid) {
+    const ageOnly2dArr = grid.cells.map(row => row.map(cell => cell.age));
+
+    // this is the nested array we need, but JSON.stringify doesn't give us
+    // nice linebreaks so we can actually see, so still print manually.
+
+    // also, in order to output strings which can be directly read as JSON,
+    // we need to ensure no trailing commas, which is why we join arrays here
+    // instead of just building a string
+
+    let outStr = "[\n";
+    const rowStrs = ageOnly2dArr.map(row => {
+        return "   [" + row.map(age => "" + age).join(",") + "]"
+    });
+    outStr += rowStrs.join(",\n")
+    outStr += "\n]";
+    console.log(outStr);
+}
+
+function createGridFromString(gridStr) {
+    const gridAges = JSON.parse(gridStr);
+    let n_j = gridAges.length;
+    let n_i = gridAges[0].length;
+
+    const grid = createGrid(n_i, n_j, createCell);
+
+    forAllCells(grid, cell => cell.age = gridAges[cell.j][cell.i]);
+
+    return grid;
+}
+
 function createCell(i, j) {
     return {
         i: i,
         j: j,
         age: 0,
         neighbors: []
-    }
+    };
 }
 
 function forAllCells(grid, fnActionOnCell) {
@@ -138,20 +171,180 @@ function seedGrid(grid, seedVals) {
 
 function printEvolutions(n_i, n_j, seedVals, maxGeneration) {
     const grid = createGrid(n_i, n_j, createCell);
-    assignNeighbors(grid); // todo: make createGrid call this instead
     seedGrid(grid, seedVals);
 
     console.log("===== Generation 1 =====");
-    showGrid(grid);
+    // showGrid(grid);
+    showGridAs2dArr(grid);
     for (var gen = 2; gen <= maxGeneration; gen++) {
         runEvolution(grid);
         console.log("===== Generation " + gen + " =====");
-        showGrid(grid);
+        // showGrid(grid);
+        showGridAs2dArr(grid);
     }
 }
 
-printEvolutions(5, 5,
+function areGridsEqual(gridA, gridB) {
+    if (gridA.n_i != gridB.n_i || gridA.n_j != gridB.n_j) return false;
+    for (var j = 0; j < gridA.n_j; j++) {
+        for (var i = 0; i < gridA.n_i; i++) {
+            if (getGridCellAt(gridA, i, j).age !== getGridCellAt(gridB, i, j).age) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function runTests() {
+    const testCase1Before = `
     [
-        [2,0,1], [2,1,1], [3,1,1], [1,2,2], [2,2,2], [3,2,1], [3,3,1]
+      [0,0,2,2,0,0,0,0,0,0],
+      [0,0,0,0,1,3,1,0,0,0],
+      [0,0,0,2,0,3,0,0,1,3],
+      [0,0,0,0,1,3,0,0,0,3],
+      [0,2,2,0,0,0,1,3,1,0],
+      [0,0,0,0,0,2,2,0,0,0],
+      [0,0,2,0,2,0,0,0,0,0],
+      [0,0,0,0,2,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
     ]
-, 2)
+    `
+    const testCase1After = `
+    [
+      [0,0,3,3,0,0,0,0,0,0],
+      [0,0,0,0,2,0,2,0,0,0],
+      [0,0,0,3,0,0,0,0,2,0],
+      [0,1,0,1,2,0,0,0,0,0],
+      [0,3,3,0,0,1,2,0,2,0],
+      [0,0,0,0,1,0,0,0,0,0],
+      [0,0,0,0,3,0,1,0,0,0],
+      [0,0,0,0,3,1,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    `
+
+    const testCase1AfterAltered = `
+    [
+      [0,0,3,3,0,0,0,0,0,0],
+      [0,0,0,0,2,0,2,0,0,0],
+      [0,0,0,3,0,0,0,0,2,0],
+      [0,1,0,1,1,0,0,0,0,0],
+      [0,3,3,0,0,1,2,0,2,0],
+      [0,0,0,0,1,0,0,0,0,0],
+      [0,0,0,0,3,0,1,0,0,0],
+      [0,0,0,0,3,1,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    `
+    
+    const testCase2Before = `
+    [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,1,1,0,0,2,0,0,0],
+      [0,0,3,3,2,0,0,0,2,0],
+      [0,1,0,0,0,0,0,0,2,0],
+      [0,0,3,0,0,1,2,0,0,0],
+      [0,0,1,3,3,3,0,0,0,0],
+      [0,0,0,1,0,1,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    `
+    
+    const testCase2After = `
+    [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,2,2,0,1,0,1,0,0],
+      [0,0,0,0,3,1,0,0,3,1],
+      [0,2,0,0,0,1,0,0,3,1],
+      [0,0,0,0,0,2,3,1,0,0],
+      [0,0,2,0,0,0,0,0,0,0],
+      [0,0,0,2,0,2,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    `
+    
+    const testCase3Before = `
+    [
+      [0,0,0,0,1,3,1,0,0,0],
+      [0,0,0,0,0,3,0,0,0,0],
+      [0,0,2,0,0,0,0,3,1,0],
+      [0,0,2,0,2,3,0,0,3,0],
+      [0,0,2,0,0,0,0,3,0,0],
+      [0,0,0,0,0,2,1,3,1,0],
+      [0,0,0,0,2,2,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    `
+    
+    const testCase3After = `
+    [
+      [0,0,0,0,2,0,2,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,1,3,0,0,0,0,0,2,0],
+      [0,0,3,0,3,0,0,0,0,0],
+      [0,1,3,0,1,1,0,0,0,0],
+      [0,0,0,1,0,0,2,0,2,0],
+      [0,0,0,0,3,0,1,0,0,0],
+      [0,0,0,0,1,1,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    `
+
+    const test1 = createGridFromString(testCase1Before);
+    const test1After = createGridFromString(testCase1After);
+    const test1AfterAltered = createGridFromString(testCase1AfterAltered);
+    const test2 = createGridFromString(testCase2Before);
+    const test2After = createGridFromString(testCase2After);
+    const test3 = createGridFromString(testCase3Before);
+    const test3After = createGridFromString(testCase3After);
+
+    function affirm(x, label) {
+        if (x) {
+            console.log("PASS " + label)
+        } else {
+            console.log("FAIL " + label)
+        }
+    }
+
+
+    affirm(!areGridsEqual(test1, test1After), "test1 initial")
+    
+    runEvolution(test1);
+    
+    affirm(areGridsEqual(test1, test1After), "test1 evolution")
+    affirm(!areGridsEqual(test1, test1AfterAltered), "test1 sanity check")
+
+
+    affirm(!areGridsEqual(test2, test2After), "test2 initial")
+
+    runEvolution(test2);
+
+    affirm(areGridsEqual(test2, test2After), "test2 evolution")
+
+    affirm(!areGridsEqual(test3, test3After), "test3 initial")
+
+    runEvolution(test3);
+
+    affirm(areGridsEqual(test3, test3After), "test3 evolution")
+
+}
+
+// printEvolutions provides an easy way to set a grid if not given a formatted string
+// printEvolutions(5, 5,
+//     [
+//         [2,0,1], [2,1,1], [3,1,1], [1,2,2], [2,2,2], [3,2,1], [3,3,1]
+//     ]
+// , 2)
+
+runTests()
